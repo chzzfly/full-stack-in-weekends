667e0bc793a7449edc6a16f3b7a2c3e921fdf6c4
zhahuya <brblmdxj_1@163.com>
2023-01-09 15:46:18 +0800

    react 组件编写页面初体验

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import "./style.css";
+
 function App() {
-  return <h1>Hello World!</h1>;
+  return (
+    <header className="header">
+      <div className="logo">
+        <img src="./logo.png" alt="logo" />
+        <h1>Today I Learned</h1>
+      </div>
+      <button className="btn btn-large shareAFact">Share a fact</button>
+    </header>
+  );
 }
 
 export default App;
+
+// function App() {
+//   return <h1>Hello World!</h1>;
+// }
+
+// export default App;
